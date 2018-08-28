2412d3353849efe6264837cbfff4b9f0d60b8764
Oskar Sharipov <oskar.sharipov@tuta.io>
2018-08-28 10:04:04 +0300

    Add smart files uploading
    Now files aren't uploading again

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ REPLAYCING_TEXT = 'мяу'
 END_TEXT = '^^'
 help_text = """Напиши любое слово этому боту, а он добавит туда немного няшности ^^
 """
+file_id = None
 
 bot = telebot.TeleBot(token)
 
@@ -21,7 +22,9 @@ def help_message(message):
 @bot.message_handler(commands=['start'])
 def start_message(message):
     help_mesasge(message)
-    bot.send_photo(message.chat.id, open('start.png', 'rb'))
+    file_id = bot.send_photo(
+            message.chat.id,
+            file_id or open('start.png', 'rb'))
 
 
 @bot.message_handler(content_types=["text"])
